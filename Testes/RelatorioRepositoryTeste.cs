000d8479098285479a8e37afe346b20eb9bb01fe
Arthur Castro <arthur.castro@supermix.com.br>
2022-08-31 00:14:33 -0300

    TEST: relatorio teste

---
+++ Testes/RelatorioRepositoryTeste.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using PGALtda.Data;
+using PGALtda.Repositorios;
+using Testes.Helper;
+using Testes.TesteFixture;
+using Xunit;
+using FluentAssertions;
+using System;
+
+namespace Testes
+{
+    [Collection(nameof(UnidadeRepositoryCollection))]
+    public class RelatorioRepositoryTeste
+    {
+        private readonly RelatorioRepositoryTesteFixture _relatorioTesteFixture;
+        private readonly RelatorioRepository _funcionarioRepository;
+
+        public RelatorioRepositoryTeste(RelatorioRepositoryTesteFixture relatorioTesteFixture)
+        {
+            _relatorioTesteFixture = relatorioTesteFixture;
+            _funcionarioRepository = relatorioTesteFixture.ObterRelatorioRepository();
+        }
+        [Fact]
+        public void Cadastrar()
+        {
+        }
+        [Fact]
+        public void Cadastrar_JaExistente()
+        {
+        }
+
+    }
+}
